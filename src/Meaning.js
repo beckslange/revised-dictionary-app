261415f535da53f4273b5a5ec2aaa3b357826cfb
beckslange <beckslange96@gmail.com>
2026-08-14 13:50:04 -0600

    Added styling to Synonyms.css

--- src/Meaning.js
+++ src/Meaning.js
@@ -3,42 +3,53 @@ import Synonyms from "./Synonyms";
 import "./Meaning.css";
 
 export default function Meaning({ meaning }) {
+  const displayedDefinitions = meaning.definitions.slice(0, 5);
+  const hasExamples = displayedDefinitions.some(function (definition) {
+    return definition.example;
+  });
+
   return (
-    <div className="Meaning">
-      <div className="meaning-left">
-        <h3 className="part-of-speech">{meaning.partOfSpeech}</h3>
+    <>
+      <div className="Meaning">
+        <div className="meaning-left">
+          <h3 className="part-of-speech">{meaning.partOfSpeech}</h3>
 
-        <ol className="definitions">
-          {meaning.definitions.slice(0, 5).map(function (definition, index) {
-            return (
-              <li className="definition" key={index}>
-                {definition.definition}
-              </li>
-            );
-          })}
-        </ol>
+          <ol className="definitions">
+            {displayedDefinitions.map(function (definition, index) {
+              return (
+                <li className="definition" key={index}>
+                  {definition.definition}
+                </li>
+              );
+            })}
+          </ol>
+        </div>
+
+        <div className="meaning-right">
+          <h4 className="examples-heading">
+            <strong>Used in a sentence</strong>
+          </h4>
+
+          {hasExamples ? (
+            <ul className="examples">
+              {displayedDefinitions.map(function (definition, index) {
+                if (!definition.example) {
+                  return null;
+                }
+
+                return (
+                  <li className="example" key={index}>
+                    {definition.example}
+                  </li>
+                );
+              })}
+            </ul>
+          ) : (
+            <p className="no-examples">No example sentences available</p>
+          )}
+        </div>
       </div>
-
-      <div className="meaning-right">
-        <h4 className="examples-heading">
-          <strong>Used in a sentence</strong>
-        </h4>
-        <ul className="examples">
-          {meaning.definitions.slice(0, 5).map(function (definition, index) {
-            if (!definition.example) {
-              return null;
-            }
-
-            return (
-              <li className="example" key={index}>
-                {definition.example}
-              </li>
-            );
-          })}
-        </ul>
-
-        <Synonyms synonyms={meaning.synonyms} />
-      </div>
-    </div>
+      <Synonyms synonyms={meaning.synonyms} />
+    </>
   );
 }
